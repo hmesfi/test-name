6c357be836433ca8ec19a43a4fcb09772a396248
nadjek01 <119895118+nadjek01@users.noreply.github.com>
2023-02-28 22:08:47 -0500

    working on the game handler and getting the sus meter to work and fixing up the enemy

--- unity_thief/Assets/Scripts/GameHandler.cs
+++ unity_thief/Assets/Scripts/GameHandler.cs
@@ -4,100 +4,107 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class GameHandler : MonoBehaviour {
+public class GameHandler : MonoBehaviour
+{
 
-      private GameObject player;
-      public static int playerSus = 0;
-      public int StartPlayerSus = 0;
-      public GameObject susText;
+    private GameObject player;
+    private GameObject enemy;
+    public static int playerSus = 0;
+    public int StartPlayerSus = 0;
+    public GameObject susText;
 
-      public static int gotTokens = 0;
-      //public GameObject tokensText;
+    public static int gotTokens = 0;
+    //public GameObject tokensText;
 
-      public bool isDefending = false;
+    public bool isDefending = false;
 
-      //public static bool stairCaseUnlocked = false;
-      //this is a flag check. Add to other scripts: GameHandler.stairCaseUnlocked = true;
+    //public static bool stairCaseUnlocked = false;
+    //this is a flag check. Add to other scripts: GameHandler.stairCaseUnlocked = true;
 
-      private string sceneName;
+    private string sceneName;
 
-      public bool hasGoldKey = true;
+    public bool hasGoldKey = true;
 
-      void Start(){
-            player = GameObject.FindWithTag("Player");
-            sceneName = SceneManager.GetActiveScene().name;
-            //if (sceneName=="MainMenu"){ //uncomment these two lines when the MainMenu exists
-                  playerSus = StartPlayerSus;
-            //}
-            updateStatsDisplay();
-      }
-
-      public void playerGetTokens(int newTokens){
-            gotTokens += newTokens;
-            updateStatsDisplay();
-      }
-
-      public void playerGetFound(int damage){
-           if (isDefending == false){
-                  playerSus -= damage;
-                  if (playerSus >=0){
-                        updateStatsDisplay();
-                  }
-                  if (damage > 0){
-                       player.GetComponent<PlayerFound>().playerHit();       //play Found animation
-            }
-            }
-
-           if (playerSus < StartPlayerSus){
-                  playerHealth = StartPlayerHealth;
-                  updateStatsDisplay();
-            }
-
-           if (playerHealth >= 5){
-                  playerHealth = 0;
-                  updateStatsDisplay();
-                  playerDies();
-            }
-      }
-
-      public void updateStatsDisplay(){
-        Text healthTextTemp = susText.GetComponent<Text>();
-        healthTextTemp.text = "SUS LEVEL: " + playerHealth;
-
-        Text tokensTextTemp = tokensText.GetComponent<Text>();
-        tokensTextTemp.text = "GOLD: " + gotTokens;
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+        enemy = GameObject.FindWithTag("Enemy");
+        sceneName = SceneManager.GetActiveScene().name;
+        //if (sceneName=="MainMenu"){ //uncomment these two lines when the MainMenu exists
+        playerSus = StartPlayerSus;
+        //}
+        updateStatsDisplay();
     }
 
-      public void playerDies(){
-            //player.GetComponent<PlayerHurt>().playerDead();       //play Death animation
-            StartCoroutine(DeathPause());
-      }
+    public void playerGetTokens(int newTokens)
+    {
+        gotTokens += newTokens;
+        updateStatsDisplay();
+    }
 
-      IEnumerator DeathPause(){
-            //player.GetComponent<PlayerMove>().isAlive = false;
-            //player.GetComponent<PlayerJump>().isAlive = false;
-            yield return new WaitForSeconds(1.0f);
-            SceneManager.LoadScene("EndLose");
-      }
+    public void playerGetFound(int damage)
+    {
+        //if (enemy.
 
-      public void StartGame() {
-            SceneManager.LoadScene("Level1");
-      }
+        //   if (playerSus < StartPlayerSus)
+        //    {
+        //        playerSus = StartPlayerSus;
+        //        updateStatsDisplay();
+        //    }
 
-      public void RestartGame() {
-            SceneManager.LoadScene("MainMenu");
-            playerHealth = StartPlayerHealth;
-      }
+        //if (playerSus >= 5)
+        //{
+        //    playerSus = 0;
+        //    updateStatsDisplay();
+        //    playerDies();
+        //}
+    }
 
-      public void QuitGame() {
-                #if UNITY_EDITOR
-                UnityEditor.EditorApplication.isPlaying = false;
-                #else
+    public void updateStatsDisplay()
+    {
+        Text SusTextTemp = susText.GetComponent<Text>();
+        SusTextTemp.text = "SUS LEVEL: " + playerSus;
+
+        //Text tokensTextTemp = tokensText.GetComponent<Text>();
+        //tokensTextTemp.text = "GOLD: " + gotTokens;
+    }
+
+    public void playerDies()
+    {
+        //player.GetComponent<PlayerHurt>().playerDead();       //play Death animation
+        //StartCoroutine(DeathPause());
+    }
+
+    IEnumerator DeathPause()
+    {
+        //player.GetComponent<PlayerMove>().isAlive = false;
+        //player.GetComponent<PlayerJump>().isAlive = false;
+        yield return new WaitForSeconds(1.0f);
+        SceneManager.LoadScene("EndLose");
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene("MainMenu");
+        playerSus = StartPlayerSus;
+    }
+
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
                 Application.Quit();
-                #endif
-      }
+#endif
+    }
 
-      public void Credits() {
-            SceneManager.LoadScene("Credits");
-      }
+    public void Credits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
 }
