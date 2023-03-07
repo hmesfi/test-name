bdc5337fe70461467cee81c77b2c5c11c1149c3b
hmesfi <107152570+hmesfi@users.noreply.github.com>
2023-03-07 17:41:32 -0500

    deleted safe key , added obstacles, fixed key sound

--- unity_thief/Assets/Scripts/SafeInteraction.cs
+++ unity_thief/Assets/Scripts/SafeInteraction.cs
@@ -1,31 +1,33 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
 
-public class SafeInteraction : MonoBehaviour
-{
-    private GameHandler gameHandler;
-    public GameObject safeArt;
+// public class SafeKeyInteraction : MonoBehaviour
+// {
+//     private GameHandler gameHandler;
+//     public GameObject safekeyArt;
 
 
-    void Start(){
-        if (GameObject.FindWithTag("GameHandler") != null){
-            gameHandler = GameObject.FindWithTag("GameHandler").GetComponent<GameHandler>();
-        }
-    }
+//     void Start(){
+//         if (GameObject.FindWithTag("GameHandler") != null){
+//             gameHandler = GameObject.FindWithTag("GameHandler").GetComponent<GameHandler>();
+//         }
+//         safekeyArt.SetActive(false);
+//     }
 
-    void Update(){
-        if (gameHandler.hasSafeKey == true) {
-            // anim.SetBool("Glow", true);
-        }
-    }
+//     void Update(){
+//         if (gameHandler.hasGoldKey == true) {
+//             safekeyArt.SetActive(true);
+//         }
+//     }
 
-    public void OnTriggerEnter2D(Collider2D other)
-    {
-        if ((other.gameObject.tag == "Player") && gameHandler.hasSafeKey == true ){
-            // open safe UI where player has to drag money off the screen
-        }
-    }
+//     public void OnTriggerEnter2D(Collider2D other)
+//     {
+//         if ((other.gameObject.tag == "Player") && gameHandler.hasGoldKey == true ){
+//             safekeyArt.SetActive(false);
+//             gameHandler.hasSafeKey = true;
+//         }
+//     }
 
   
-}
+// }
--- unity_thief/Assets/Scripts/SafeKeyInteraction.cs
+++ unity_thief/Assets/Scripts/SafeKeyInteraction.cs
@@ -2,31 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class KeyInteract : MonoBehaviour
+public class SafeKeyInteraction : MonoBehaviour
 {
     private GameHandler gameHandler;
-    public AudioClip keyPickUpClip;
+    public GameObject safekeyArt;
     public AudioSource keyPickUpSFX;
-    public GameObject keyArt;
+
 
     void Start(){
         if (GameObject.FindWithTag("GameHandler") != null){
             gameHandler = GameObject.FindWithTag("GameHandler").GetComponent<GameHandler>();
         }
+        safekeyArt.SetActive(false);
         keyPickUpSFX = GetComponent<AudioSource>();
-           Debug.Log("lalalala");
     }
 
-    // Update is called once per frame
+    void Update(){
+        if (gameHandler.hasGoldKey == true) {
+            safekeyArt.SetActive(true);
+        }
+    }
+
     public void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.tag == "Player"){
-            Debug.Log("u stepped on the key");
-            keyArt.SetActive(false);
+        if ((other.gameObject.tag == "Player") && gameHandler.hasGoldKey == true ){
+            safekeyArt.SetActive(false);
+            gameHandler.hasSafeKey = true;
             GetComponent<Collider2D>().enabled = false;
             keyPickUpSFX.Play();
-            gameHandler.hasGoldKey = true;
-            gameHandler.DisplayKeys(true);
             StartCoroutine(DelayDestroy());
         }
     }
@@ -34,6 +37,5 @@ public class KeyInteract : MonoBehaviour
     IEnumerator DelayDestroy(){
         yield return new WaitForSeconds(0.5f);
         Destroy(gameObject);
-    }
-
+    } 
 }
